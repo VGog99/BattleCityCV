5dcec8ea2e242c4b34d6e67721ea95af41548da2
Cristi <cristian.m99a@gmail.com>
2019-11-10 20:30:28 +0200

    //ignore - rezolvat vcxproj corrupted

--- battlecity/Bush.cpp
+++ battlecity/Bush.cpp
@@ -1,7 +1,5 @@
 #include "Bush.h"
 
-
-
 Bush::Bush()
 {
 	setCanAdvance(true);
--- battlecity/Main.cpp
+++ battlecity/Main.cpp
@@ -1,35 +1,14 @@
-<<<<<<< Updated upstream
 #include <SFML\Graphics.hpp>
-=======
+
 #include <iostream>
 #include "SDK.h"
->>>>>>> Stashed changes
 
 
 int main()
 {
-<<<<<<< Updated upstream
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
 
-	while (window.isOpen())
-	{
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed)
-				window.close();
-		}
-
-		window.clear();
-
-		//draw stuff
-
-		window.display();
-	}
-=======
 	GameBoard game;
 	game.draw();
->>>>>>> Stashed changes
 
 	return 0;
 }
