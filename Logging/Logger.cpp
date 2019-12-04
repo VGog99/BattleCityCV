f725dd5c98e574101bcfb9997c67270eb66bb636
GogV <vladgogu14@gmail.com>
2019-12-04 19:37:12 +0200

    DLL
    Am adaugat un DLL jocului care da actual mesaje de nivel info sau mai mare.

---
+++ Logging/Logger.cpp
@@ -0,0 +1,34 @@
+#include "Logger.h"
+
+
+
+Logger::Logger(std::ostream& out, Level minLogLevel) :m_out(out), m_minLogLevel(minLogLevel)
+{
+}
+void Logger::Log(const std::string& message, Level level)
+{
+	if (level < m_minLogLevel)
+		return;
+	m_out << message << "\n";
+	switch (level)
+	{
+	case Logger::Level::Debug:
+		m_out << "[Debug]";
+		break;
+	case Logger::Level::Info:
+		m_out << "[Info]";
+		break;
+	case Logger::Level::Warning:
+		m_out << "[Warning]";
+		break;
+	case Logger::Level::Error:
+		m_out << "[Error]";
+		break;
+	}
+	m_out << message << std::endl;
+	//m_out.flush();
+}
+
+Logger::~Logger()
+{
+}
--- battlecity/Main.cpp
+++ battlecity/Main.cpp
@@ -1,9 +1,14 @@
 #include <SFML\Graphics.hpp>
 #include <iostream>
 #include "SDK.h"
+#include "../Logging/Logger.h"
+#include <fstream>
 
 int main()
 {
+	std::ofstream logFile("log.log", std::ios::app);
+	Logger logger(std::cout, Logger::Level::Info);
+	logger.Log("Application started", Logger::Level::Info);
 	srand(time(0));
 	GameBoard game;
 	game.draw();
