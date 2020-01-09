8d326c5e55b4bee1c572f8240da185736e000680
Cristi <cristian.m99a@gmail.com>
2020-01-10 00:29:05 +0200

    Fixed unit testing
    acum merge cu SFML, adaugata o metoda de test

--- BCUnitTesting/BCUnitTesting.cpp
+++ BCUnitTesting/BCUnitTesting.cpp
@@ -8,13 +8,20 @@ namespace BCUnitTesting
 {
 	TEST_CLASS(BCUnitTesting)
 	{
-	public:
-		
-		TEST_METHOD(Constructor)
+	public:	
+
+		TEST_METHOD(TestMethod1)
 		{
 			Tank tank;
 			tank.setTankDirection('0');
 			Assert::AreEqual(tank.getTankDirection(), '0');
 		}
+
+		TEST_METHOD(TestMethod2) {
+			Tank tank;
+			tank.setTankSpeed(15.f);
+			Assert::AreEqual(tank.getTankSpeed(), 15.f);
+		}
+
 	};
 }
